8456b438acb32644ac4952570ae612cbed9d3376
Harshit Saxena <146431234+saxenaharshit63@users.noreply.github.com>
2024-10-14 14:23:52 +0530

    Create EvenSum.c

---
+++ EvenSum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum=0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i=2; i<=n; i+=2) {
+        sum += i;
+    }
+    printf("%d", sum);
+    
+    return 0;
+}
